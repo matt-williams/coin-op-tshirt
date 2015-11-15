fca1258106593b89cdd9595ee07cb8efddbcc175
Matt Williams <matwilliams@hotmail.com>
2015-11-15 05:00:56 +0000

    Poll correct URL and turn on EL when payment received

--- esp8266/user/user_main.c
+++ esp8266/user/user_main.c
@@ -6,15 +6,29 @@
 #include "user_interface.h"
 #include "httpclient.h"
 
+#define ELGPIO 2
+
+static ETSTimer el_timer;
 static ETSTimer poll_timer;
+
+static void ICACHE_FLASH_ATTR el_timer_callback(void* arg) {
+  os_printf("Turning off EL\n");
+  gpio_output_set(0, (1<<ELGPIO), (1<<ELGPIO), 0);
+}
 
 static void ICACHE_FLASH_ATTR poll_http_callback(char* response, int http_status, char* full_response) {
   os_printf("GET response has status %d and body \"%s\"\n", http_status, response);
+  if (http_status == 200) {
+    os_printf("Turning on EL\n");
+    gpio_output_set((1<<ELGPIO), 0, (1<<ELGPIO), 0);
+    os_timer_disarm(&el_timer);
+    os_timer_arm(&el_timer, 10000, 0);
+  }
 }
 
 static void ICACHE_FLASH_ATTR poll_timer_callback(void* arg) {
   os_printf("Polling\n");
-  http_get("http://wtfismyip.com/text", "", poll_http_callback);
+  http_get("http://coin-op-tshirt.herokuapp.com/simplify/poll", "", poll_http_callback);
 }
 
 // WiFi event callback
@@ -55,6 +69,9 @@ void ICACHE_FLASH_ATTR user_init()
 
   // Enable GPIO
   gpio_init();
+  PIN_FUNC_SELECT(PERIPHS_IO_MUX_GPIO2_U, FUNC_GPIO2);
+  PIN_FUNC_SELECT(PERIPHS_IO_MUX_GPIO0_U, FUNC_GPIO0);
+  gpio_output_set(0, 0, (1<<ELGPIO), 0);
 
   // Set station mode
   wifi_set_opmode_current(STATION_MODE);
@@ -67,9 +84,11 @@ void ICACHE_FLASH_ATTR user_init()
   // Set an event handler for WiFi events
   wifi_set_event_handler_cb(wifi_callback);
 
-  // Setup timer, but don't start it yet
+  // Setup poll and EL timers, but don't start them yet
   os_timer_disarm(&poll_timer);
   os_timer_setfn(&poll_timer, poll_timer_callback, NULL);
+  os_timer_disarm(&el_timer);
+  os_timer_setfn(&el_timer, el_timer_callback, NULL);
 
   os_printf("user_init() complete!\n\r");
 }
